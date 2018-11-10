04ec29db1acc259c22a419363e6b6448149ca931
Michael Quinn <mike88quinn@gmail.com>
2018-11-10 17:12:15 -0500

    Bugs

--- bridger.js
+++ bridger.js
@@ -1,3 +1,30 @@
+window.fbAsyncInit = function() {
+	FB.init({
+		appId      : '698670277184912',
+		cookie     : true,  // enable cookies to allow the server to access 
+												// the session
+		xfbml      : true,  // parse social plugins on this page
+		version    : 'v3.2' // use graph api version 3.2
+	});
+	
+	// Now that we've initialized the JavaScript SDK, we call 
+	// FB.getLoginStatus().  This function gets the state of the
+	// person visiting this page and can return one of three states to
+	// the callback you provide.  They can be:
+	//
+	// 1. Logged into your app ('connected')
+	// 2. Logged into Facebook, but not your app ('not_authorized')
+	// 3. Not logged into Facebook and can't tell if they are logged into
+	//    your app or not.
+	//
+	// These three cases are handled in the callback function.
+	
+	FB.getLoginStatus(function(response) {
+		statusChangeCallback(response);
+	});
+
+};
+
 // This is called with the results from from FB.getLoginStatus().
 function statusChangeCallback(response) {
 	console.log('statusChangeCallback');
@@ -26,32 +53,7 @@ function checkLoginState() {
 	});
 }
 
-window.fbAsyncInit = function() {
-	FB.init({
-		appId      : '698670277184912',
-		cookie     : true,  // enable cookies to allow the server to access 
-												// the session
-		xfbml      : true,  // parse social plugins on this page
-		version    : 'v3.2' // use graph api version 3.2
-	});
-	
-	// Now that we've initialized the JavaScript SDK, we call 
-	// FB.getLoginStatus().  This function gets the state of the
-	// person visiting this page and can return one of three states to
-	// the callback you provide.  They can be:
-	//
-	// 1. Logged into your app ('connected')
-	// 2. Logged into Facebook, but not your app ('not_authorized')
-	// 3. Not logged into Facebook and can't tell if they are logged into
-	//    your app or not.
-	//
-	// These three cases are handled in the callback function.
-	
-	FB.getLoginStatus(function(response) {
-		statusChangeCallback(response);
-	});
 
-};
 
 
 
