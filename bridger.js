406b93e6331bd4f13695f45d1892e158421ae57f
Michael Quinn <mike88quinn@gmail.com>
2018-11-10 20:29:33 -0500

    check login status

--- bridger.js
+++ bridger.js
@@ -76,6 +76,31 @@ function statusChangeCallback(response) {
 // code below.
 function checkLoginState() {
 	FB.getLoginStatus(function(response) {
-		statusChangeCallback(response);
-	});
+  if (response.status === 'connected') {
+    // The user is logged in and has authenticated your
+    // app, and response.authResponse supplies
+    // the user's ID, a valid access token, a signed
+    // request, and the time the access token 
+    // and signed request each expire.
+		document.write("boom boom");
+    var uid = response.authResponse.userID;
+    var accessToken = response.authResponse.accessToken;
+  } else if (response.status === 'authorization_expired') {
+    // The user has signed into your application with
+    // Facebook Login but must go through the login flow
+    // again to renew data authorization. You might remind
+    // the user they've used Facebook, or hide other options
+    // to avoid duplicate account creation, but you should
+    // collect a user gesture (e.g. click/touch) to launch the
+    // login dialog so popup blocking is not triggered.
+  } else if (response.status === 'not_authorized') {
+    // The user hasn't authorized your application.  They
+    // must click the Login button, or you must call FB.login
+    // in response to a user gesture, to launch a login dialog.
+  } else {
+    // The user isn't logged in to Facebook. You can launch a
+    // login dialog with a user gesture, but the user may have
+    // to log in to Facebook before authorizing your application.
+  }
+ });
 }
