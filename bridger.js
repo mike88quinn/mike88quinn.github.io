76dbac87168a482c8567d3e4a353fba579c1b204
Michael Quinn <mike88quinn@gmail.com>
2018-11-12 10:22:32 -0500

    Facebook button fix

--- bridger.js
+++ bridger.js
@@ -23,16 +23,6 @@ FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
 });
 
-{
-    status: 'connected',
-    authResponse: {
-        accessToken: '...',
-        expiresIn:'...',
-        signedRequest:'...',
-        userID:'...'
-    }
-}
-
 function checkLoginState() {
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
