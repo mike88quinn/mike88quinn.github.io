733b502c4503fe6a79f1879b0e046d00d7b31a6b
Michael Quinn <mike88quinn@gmail.com>
2018-11-13 13:33:03 -0500

    Update profile pic display

--- bridger.js
+++ bridger.js
@@ -96,6 +96,6 @@ function checkLoginState() {
 
 function getInfo() {
 	FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id,picture.width(150).height(150)'}, function(response) {
-		document.getElementById('status').innerHTML = "<img src='" + response.picture.data.url + "'>";
+		document.getElementById('status').innerHTML = response.picture.data.url;
 	});
 }
