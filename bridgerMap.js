1c0e3671279b4c6a61a090c7c52d6bfc64c54d9c
Michael Quinn <mike88quinn@gmail.com>
2018-11-29 14:46:17 -0500

    map

--- bridgerMap.js
+++ bridgerMap.js
@@ -25,42 +25,34 @@ function initMap() {
 			var email = markerElem.getAttribute('email');
 			var type = markerElem.getAttribute('type');
 			var address = markerElem.getAttribute('address');
-			var point;
 			geocoder.geocode( { 'address': address}, function(results, status) {
 				if (status == 'OK') { 
-					point = results[0].geometry.location;
+					var infowincontent = document.createElement('div');
+					var strong = document.createElement('strong');
+					strong.textContent = name
+			
+					var text = document.createElement('text');
+					text.textContent = email
+					infowincontent.appendChild(text);
+					
+					infowincontent.appendChild(strong);
+					infowincontent.appendChild(document.createElement('br'));
+					infowincontent.appendChild(text);
+					
+					var icon = customLabel[type] || {};
+					var marker = new google.maps.Marker({
+						map: map,
+						position: results[0].geometry.location;
+						label: icon.label
+					});
+					
+					marker.addListener('click', function() {
+						infoWindow.setContent(infowincontent);
+						infoWindow.open(map, marker);
+					});
 				} else {
 					alert('Geocode was not successful for the following reason: ' + status);
 				}
-				
-				var infowincontent = document.createElement('div');
-				var strong = document.createElement('strong');
-				strong.textContent = name
-		
-				var text = document.createElement('text');
-				text.textContent = email
-				infowincontent.appendChild(text);
-				
-				infowincontent.appendChild(strong);
-				infowincontent.appendChild(document.createElement('br'));
-				infowincontent.appendChild(text);
-				
-				var icon = customLabel[type] || {};
-				var marker = new google.maps.Marker({
-					map: map,
-					position: point,
-					label: icon.label
-				});
-				
-				marker.addListener('click', function() {
-					infoWindow.setContent(infowincontent);
-					infoWindow.open(map, marker);
-				});
-			/*var point = new google.maps.LatLng(
-					parseFloat(markerElem.getAttribute('lat')),
-					parseFloat(markerElem.getAttribute('lng')));*/
-	
-			
 			});
 		});
 	});
