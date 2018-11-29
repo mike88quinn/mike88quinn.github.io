9ed04892dc48a70ac0601a6ede7ac2610707cc49
Michael Quinn <mike88quinn@gmail.com>
2018-11-29 13:54:27 -0500

    deleted garbage from map.js

--- bridgerMap.js
+++ bridgerMap.js
@@ -106,22 +106,3 @@ function downloadUrl(url, callback) {
 }
 
 function doNothing() {}
-------------------------------------------------------------------------------------------
-
-  map = new google.maps.Map(document.getElementById('map'), mapOptions);
-}
-
-function codeAddress() {
-  var address = document.getElementById('address').value;
-  geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == 'OK') {
-      map.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
