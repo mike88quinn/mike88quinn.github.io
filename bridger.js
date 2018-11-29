7f9bf1f676d9a9af1aa781b127b6d632a7543885
Michael Quinn <mike88quinn@gmail.com>
2018-11-29 16:13:54 -0500

    fix user location

--- bridger.js
+++ bridger.js
@@ -117,10 +117,10 @@ function login() {
 		
 // getting basic user info
 function getFacebookPhoto() {
-	FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id,email,location,picture.width(150).height(150)'}, function(response) {
+	FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id,email,user_location,picture.width(150).height(150)'}, function(response) {
 		document.getElementById('userPhoto').innerHTML = "<img src='" + response.picture.data.url + "'>";
 		document.getElementById('name').innerHTML = response.name;
-		document.getElementById('location').innerHTML = response.location;
+		document.getElementById('location').innerHTML = response.user_location;
 		document.getElementById('email').innerHTML = response.email;
 		document.getElementById('id').innerHTML = response.id;
 	});
