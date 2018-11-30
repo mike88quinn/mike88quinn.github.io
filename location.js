89e40dffb3be564ca64a3e3c1722cede12df18b7
Michael Quinn <mike88quinn@gmail.com>
2018-11-29 22:32:59 -0500

    test map

--- location.js
+++ location.js
@@ -1,29 +1,8 @@
 // getting basic user info
-function getUserInfo() {
-	var geocoder = new google.maps.Geocoder;
-	var pos;
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(position) {
-			pos = {
-				lat: position.coords.latitude,
-				lng: position.coords.longitude
-			};
-		}, function() {
-			handleLocationError(true, infoWindow, map.getCenter());
-		});
-	} else {
-		// Browser doesn't support Geolocation
-		handleLocationError(false, infoWindow, map.getCenter());
-	}
-	geocoder.geocode({'location': pos}, function(results, status) {
-		if (status === 'OK') {
-      if (results[0]) {
-				document.getElementById('loca').innerHTML = results[0].formatted_address;
-      } else {
-        window.alert('No results found');
-      }
-    } else {
-      window.alert('Geocoder failed due to: ' + status);
-    }
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('loca'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 12,
   });
+  var infoWindow = new google.maps.InfoWindow;
 }
