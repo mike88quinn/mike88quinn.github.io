3083e9a99cc005eb1066367eaad2067a9d1c399d
Michael Quinn <mike88quinn@gmail.com>
2018-11-29 22:30:09 -0500

    geocoder

--- location.js
+++ location.js
@@ -1,6 +1,6 @@
 // getting basic user info
 function getUserInfo() {
-	var geocoder = new google.maps.Geocoder();
+	var geocoder = new google.maps.Geocoder;
 	var pos;
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {
@@ -15,10 +15,10 @@ function getUserInfo() {
 		// Browser doesn't support Geolocation
 		handleLocationError(false, infoWindow, map.getCenter());
 	}
-	geocoder.geocode( { 'location': pos}, function(results, status) {
+	geocoder.geocode({'location': pos}, function(results, status) {
 		if (status === 'OK') {
       if (results[0]) {
-				document.getElementById('location').innerHTML = results[0].formatted_address;
+				document.getElementById('loca').innerHTML = results[0].formatted_address;
       } else {
         window.alert('No results found');
       }
