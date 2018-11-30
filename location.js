596852b490ce9091c0925cbcdbe0da5ab17281d1
Michael Quinn <mike88quinn@gmail.com>
2018-11-30 10:07:39 -0500

    Data changes
    Deleted files that are not needed and changed format in xml file

--- location.js
+++
@@ -1,8 +0,0 @@
-// getting basic user info
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('loca'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 12,
-  });
-  var infoWindow = new google.maps.InfoWindow;
-}
